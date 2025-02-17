4b68f4911027116df66106f31458e838db21992e
elizabethdinella <elizabeth.a.dinella@gmail.com>
2025-02-16 23:41:18 -0500

    part 2 starter code

---
+++ hws/hw02/Jaccard.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.*;
+
+public class Jaccard {
+
+    //TODO: put the targt test prefix here
+    String TARGET_PREFIX = "";
+
+    public static Set<String> getWords(String line) {
+        return new HashSet<>(Arrays.asList(line.split("\\s+"))); 
+    }
+
+    public static double jaccard(Set t1, Set t2) {
+        //TODO: implement jaccard similarity score
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+        String filePath = "corpus.txt"; 
+        String assertPath = "corpusAsserts.txt";
+
+        Scanner scanner = new Scanner(new File(filePath));
+        Scanner assertScanner = new Scanner(new File(assertPath));
+
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            String assertion = assertScanner.nextLine();
+
+            Set<String> words = Jaccard.getWords(line);
+
+            //TODO: create a data structure to store a set of words and its corresponding asertion. 
+        }
+
+        String retrievedAsesrtion = "";
+        double bestSimilarity = 0;
+
+        //TODO: Find the most similar document 
+        for (/*each corpusAssertion,corpusTest in the corpus*/) {
+            double similarity = Jaccard.jaccard(TARGET_PREFIX, corpusTest);
+            
+            if (similarity > bestSimilarity) {
+                retrievedAssertion = corpusAssertion;
+                bestSimilarity = similarity;
+            }
+        }
+
+        System.out.println(retrievedAssertion);
+    }
+}
