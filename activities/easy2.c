46dfbd03d790f87f199c9b89e01b3a23932ae7cb
elizabethdinella <elizabeth.a.dinella@gmail.com>
2025-03-04 18:12:47 -0500

    easy

---
+++ activities/easy2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+  char input[65536];
+  fgets(input, sizeof(input), stdin);
+  int x = 13;
+  int z = 21;
+  int y = 0;
+
+  if (strlen(input) > 10 && input[5] == 'a') {
+    z = x / y;
+  }
+
+  return 0;
+}
